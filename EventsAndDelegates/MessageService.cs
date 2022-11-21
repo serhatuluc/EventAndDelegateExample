38b4dab175dcac5a1c10619b19577bc81bcd724e
serhatuluc <99399280+serhatuluc@users.noreply.github.com>
2022-11-21 20:51:42 +0300

    Event and delegate is  created

---
+++ EventsAndDelegates/MessageService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class MessageService
+    {
+        public void OnVideoEncoded(object source, VideoEventArgs e)
+        {
+            Console.WriteLine("MessageService: Sending a text message..." + e.Video.Title);
+        }
+    }
+}
---
+++ EventsAndDelegates/VideoEncoder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class VideoEncoder
+    {
+
+        //1-Define a delegate
+        //2-Define an event based on that delegate
+        //3-Raise the event
+
+        //Agreement between publisher and subscribers
+        public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+        public event VideoEncodedEventHandler VideoEncoded;
+        public void Encode(Video video)
+        {
+            Console.WriteLine("Encoding video");
+            Thread.Sleep(3000);
+            OnVideoEncoded(video);
+        }
+        
+        //This method is responsible of notifying all subscribers
+        protected virtual void OnVideoEncoded(Video video)
+        {
+            if(VideoEncoded != null)
+            {
+                VideoEncoded(this, new VideoEventArgs() { Video = video});
+            }
+        }
+    }
+}
